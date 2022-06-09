9041fed8b9d7d8472e8b2a214fa27f4035586a5d
Tsering Tsering <dtsering2@gmail.com>
2022-06-09 17:53:10 -0400

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {name: "NoName", streetAddress:"NoAddress"};
+//Nondestructive
+function updateEmployeeWithKeyAndValue(obj,key,value){
+    return{
+        ...obj,
+        [key]:value,
+    };
+}
+//Destructive
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+    obj[key]=value;
+    return obj;
+}
+
+//Nondestructive delete
+function deleteFromEmployeeByKey(obj,key){
+    const newobj={...obj};
+    delete newobj[key];
+    return newobj;
+}
+
+//destructive delete
+function destructivelyDeleteFromEmployeeByKey(obj,key){
+  
+    delete obj[key];
+    return obj;
+}
